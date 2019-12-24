659c5ef5c588624dc340da1797c801a249e53414
wcolts2000 <wcolts2000@gmail.com>
2019-12-24 01:23:15 -0800

    register page completed and footer extracted to component

--- index.php
+++ index.php
@@ -340,41 +340,8 @@
 
   </div>
   <!--END CONTAINER -->
+  <?php include "footer.php" ?>
 
-  <!-- FOOTER -->
-  <footer class="page-footer cyan darken-2">
-    <div class="container">
-      <div class="row">
-        <div class="col l6 s12">
-          <h5 class="white-text">High Quality Printing in the Reno Area</h5>
-          <p class="grey-text text-lighten-4">Providing Reno Sparks and Carson City with high quality printing.</p>
-          <ul>
-            <li><a class="grey-text text-lighten-3" href="/large_format.php">Large Format</a></li>
-            <li><a class="grey-text text-lighten-3" href="/offset.php">Offset</a></li>
-            <li><a class="grey-text text-lighten-3" href="/garments.php">Garments</a></li>
-            <li><a class="grey-text text-lighten-3" href="/websites.php">Websites</a></li>
-          </ul>
-        </div>
-        <div class="col l4 offset-l2 s12">
-          <h5 class="white-text">Info</h5>
-          <ul>
-            <li><a class="grey-text text-lighten-3" href="/privacy_policy.php">Privacy Policy</a></li>
-            <li><a class="grey-text text-lighten-3" href="/terms.php">Terms & Conditions</a></li>
-            <li><a class="grey-text text-lighten-3" href="/faq.php">FAQ</a></li>
-          </ul>
-        </div>
-      </div>
-    </div>
-    <div class="footer-copyright">
-      <div class="container">
-        © 2019 Copyright | Hurry Up Print
-        <a class="grey-text text-lighten-4 right" href="mailto:sales@hurryupprint.com">
-          <i class="material-icons left">email</i>
-          sales@hurryupprint.com
-        </a>
-      </div>
-    </div>
-  </footer>
 
   <!--Import jQuery before materialize.js-->
   <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
--- js/index.js
+++ js/index.js
@@ -3,52 +3,53 @@ const appState = {
   password: '',
   loggedIn: false,
   loginModalOpen: false
-}
+};
 
 $(document).ready(function() {
   // NAVBAR
-    // Init nav dropdowns
-    $('.dropdown-button').dropdown({
-      belowOrigin: true,
-      alignment: 'left'
-    })
-
-    $('.button-collapse').sideNav({
-      hover: true
-    });
-
-    // TABS
-    $('.tabs').tabs();
-
-    // PARALLAX
-    $('.parallax').parallax();
-    
-    // COLLAPSIBLE
-    // $('.collapsible').collapsible()
-
-    // CAROUSEL
-    // $('.carousel').carousel();
-    // $('.carousel-slider').carousel({ fullWidth: true});
-
+  // Init nav dropdowns
+  $('.dropdown-button').dropdown({
+    belowOrigin: true,
+    alignment: 'left'
   });
 
-function handleLoginClick(ev) {
-  console.log(ev)
-  if(appState.loggedIn) {
+  $('.button-collapse').sideNav({
+    hover: true
+  });
 
-    logout()
+  // TABS
+  $('.tabs').tabs();
+
+  // PARALLAX
+  $('.parallax').parallax();
+
+  // FORM SELECTS
+  $('select').material_select();
+
+  // COLLAPSIBLE
+  // $('.collapsible').collapsible()
+
+  // CAROUSEL
+  // $('.carousel').carousel();
+  // $('.carousel-slider').carousel({ fullWidth: true});
+});
+
+function handleLoginClick(ev) {
+  console.log(ev);
+  if (appState.loggedIn) {
+    logout();
   } else {
-    login("samwise", "pass")
+    login('samwise', 'pass');
   }
 }
 
 function login(username, password) {
-  console.log(appState, username, password)
-  appState.username = username
-  appState.password = password
-  appState.loggedIn = true
-  console.log(appState)
-  LoginNavHandle()
+  console.log(appState, username, password);
+  appState.username = username;
+  appState.password = password;
+  appState.loggedIn = true;
+  console.log(appState);
+  LoginNavHandle();
 }
 
 function logout() {
@@ -56,20 +57,18 @@ function logout() {
   appState.password = '';
   appState.loggedIn = false;
   appState.loginModalOpen = false;
-  console.log(appState)
-  LoginNavHandle()
+  console.log(appState);
+  LoginNavHandle();
 }
 
 function handleFormSubmit(event) {
-  event.preventDefault()
-  console.log(event)
-  console.dir(event)
-  const URL = "http://localhost:8080/api/auth/register"
-  const registerBody = {"username": "samwise@email.com", "password": "pass"}
-  
+  event.preventDefault();
+  console.log(event);
+  console.dir(event);
+  const URL = 'http://localhost:8080/api/auth/register';
+  const registerBody = { username: 'samwise@email.com', password: 'pass' };
+
   // axios.post(URL, registerBody)
   //   .then(res => console.log("RES", res))
   //   .catch(err => console.log("ERR", err))
 }
-
-
