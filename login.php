78a639e3a6c08fec682125260a5ce2a38478f72c
wcolts2000 <wcolts2000@gmail.com>
2019-12-24 02:08:12 -0800

    login page done

--- login.php
+++ login.php
@@ -15,6 +15,50 @@
 <body>
   <?php include 'header.php'; ?>
 
+  <div class="container" style="margin: 2rem auto">
+    <div class="row">
+      <div class="col s12 m6">
+        <div class="card grey lighten-4">
+          <div class="card-content">
+            <span class="card-title">New Customer?</span>
+            <p>Please go to our
+              <a href="register.php" class="blue-grey-text">register</a>
+              page and create a new account.</p>
+          </div>
+          <div class="card-action">
+            <a class="btn waves-effect waves-light black white-text" href="register.php">Register</a>
+          </div>
+        </div>
+      </div>
+      <div class="col s12 m6">
+        <!-- <div class="card grey lighten-4"> -->
+        <form class="card grey lighten-4">
+          <div class="card-content">
+            <span class="card-title">
+              Login
+            </span>
+            <div class="row">
+              <label for="email">Email</label>
+              <input type="email" id="email" placeholder="johndoe@email.com" class="validate">
+            </div>
+            <div class="row">
+              <label for="password">Password</label>
+              <input type="password" id="password" placeholder="$k8vG3p09aB%77hn2^" class="validate">
+            </div>
+            <div class="row">
+
+              <button type="submit" class="btn yellow darken-2 black-text waves-effect waves-light">Login</button>
+            </div>
+          </div>
+        </form>
+        <!-- </div> -->
+      </div>
+    </div>
+  </div>
+
+
+  <?php include 'footer.php'; ?>
+
   <!--Import jQuery before materialize.js-->
   <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
   <script type="text/javascript" src="js/materialize.min.js"></script>
