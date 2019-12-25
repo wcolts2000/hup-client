b6d21c4a782b64a8cec15d9437ac26a8ad98b2f0
wcolts2000 <wcolts2000@gmail.com>
2019-12-24 22:03:16 -0800

    added large format main page and garments main page

--- garments.php
+++ garments.php
@@ -15,6 +15,131 @@
 <body>
   <?php include 'header.php'; ?>
 
+  <!-- HEADER -->
+  <div class="lf-header">
+    <div class="parallax-container page-header center-align">
+      <div class="parallax"><img src="img/embroidery_machines.png" alt="embroidery machines"></div>
+      <h1 class="white-text ps2 ts">Custom Garments</h1>
+      <h4 class="ps2 white-text ts">Get your logo on your uniforms and clothing.</h5>
+        <a href="/garments/silk_screen.php"
+          class="round-border btn waves-effect waves-light yellow darken-2 black-text m2">Silk Screening Here
+          <i class="material-icons black-text right">chevron_right</i>
+        </a>
+    </div>
+  </div>
+
+  <!-- CONTAINER -->
+  <div class="container">
+
+    <!-- MAIN -->
+    <main class="lf-main">
+
+      <!-- BREADCRUMBS -->
+      <nav class="yellow darken-2 m2a">
+        <div class="nav-wrapper ms2">
+          <div class="col s12">
+            <a href="/index.php" class="breadcrumb"><i class="material-icons">home</i></a>
+            <a href="/garments.php" class="breadcrumb black-text">Garments</a>
+          </div>
+        </div>
+      </nav>
+
+      <!-- PRODUCTS -->
+      <div id="category1" class="col s12">
+        <h4>Advertise Everywhere You Go.</h4>
+        <div class="row">
+          <div class="col s6">
+            <img src="img/shirt_front_print_2.png" alt="a custom silkscreened logo on a shirt front"
+              class="responsive-img">
+          </div>
+          <div class="col s6">
+            <ul class="collection">
+              <a href="/garments/silk_screen.php" class="collection-item black-text waves-effect waves-light">
+                <li>Silk Screen Printing</li>
+              </a>
+              <a href="/garments/embroidery.php" class="collection-item black-text waves-effect waves-light">
+                <li>Embroidery</li>
+              </a>
+              <a href="/garments/vinyl.php" class="collection-item black-text waves-effect waves-light">
+                <li>Vinyl</li>
+              </a>
+            </ul>
+          </div>
+        </div>
+      </div> <!-- END PRODUCTS -->
+
+    </main>
+
+    <!-- INFO DROPDOWNS -->
+    <section class="info row">
+      <div class="col s12">
+        <h5>Some Frequently Asked Questions about Garment Printing</h5>
+      </div>
+      <div class="col s12 m6">
+        <ul class="collapsible">
+          <li>
+            <div class="collapsible-header"><i class="material-icons teal-text">arrow_drop_down_circle</i>What is
+              minimum order?</div>
+            <div class="collapsible-body"><span>Depending on what type of garment service you get the minimums will
+                vary. If you are looking for silk screen printing,
+                the minimum order will be a dozen (12) garments per design. Bulk pricing breaks decrease the price per
+                unit as the order number increases on silk screen jobs.
+                Color changes are allowed on the same design but do add an additional fee. With Embroidery, you can get
+                an number of garments or accessories embroidered.
+                Digitizing fees for the artwork may apply. With vinyl presses you may get as may as you like as well,
+                but no bulk pricing will apply.</span></div>
+          </li>
+          <li>
+            <div class="collapsible-header"><i class="material-icons teal-text">arrow_drop_down_circle</i>Can i get
+              front and backs done?</div>
+            <div class="collapsible-body"><span>Depending on the garment, most areas are able to be printed or
+                embroidered on, but each location is charged accordingly.
+                if there is a size difference needed on prints between garments, additional fees may apply</span></div>
+          </li>
+          <li>
+            <div class="collapsible-header"><i class="material-icons teal-text">arrow_drop_down_circle</i>What colors
+              are offered?</div>
+            <div class="collapsible-body"><span>We can get nearly any color, but some metallics and specialty inks or
+                threads will add prices and require a custom quote.</span></div>
+          </li>
+        </ul>
+      </div>
+      <div class="col s12 m6">
+        <ul class="collapsible">
+          <li>
+            <div class="collapsible-header"><i class="material-icons teal-text">arrow_drop_down_circle</i>Is there a
+              discount the more I order?</div>
+            <div class="collapsible-body"><span>With silk screen printing, there are price breaks as varying quantities.
+                The more you get, the more you save.
+                With embroidery, the pricing is based on the size and stitch count of the graphic. With vinyl press, the
+                pricing is per unit.
+              </span></div>
+          </li>
+          <li>
+            <div class="collapsible-header"><i class="material-icons teal-text">arrow_drop_down_circle</i>What if I want
+              different colors on some?</div>
+            <div class="collapsible-body"><span>With silk screen prints, there is a $15.00 color change fee on each
+                change to the original color. With embroidery and vinyl presses,
+                there is no additional fee unless the color is special ordered or out of stock.
+              </span></div>
+          </li>
+          <li>
+            <div class="collapsible-header"><i class="material-icons teal-text">arrow_drop_down_circle</i>Can I get the
+              garments from you or do I provide them?</div>
+            <div class="collapsible-body"><span>Both options are acceptable. If you have the garments you want
+                customized already, you can ship them to us or drop them off if you are local.
+                If you need the garments supplied, we have a wide selection of brands and styles to choose from at a
+                great price.
+              </span></div>
+          </li>
+        </ul>
+      </div>
+    </section>
+
+  </div> <!-- END CONTAINER -->
+
+  <?php include 'footer.php' ?>
+
   <!--Import jQuery before materialize.js-->
   <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
   <script type="text/javascript" src="js/materialize.min.js"></script>
--- large_format.php
+++ large_format.php
@@ -15,6 +15,144 @@
 <body>
   <?php include 'header.php'; ?>
 
+  <!-- HEADER -->
+  <div class="lf-header">
+    <div class="parallax-container page-header center-align">
+      <div class="parallax"><img src="img/printing_banner.jpg"></div>
+      <h1 class="white-text ps2 ts">Large Format Printing</h1>
+      <h4 class="ps2 white-text ts">Get a banner for your next event.</h5>
+        <a href="/large_format/banners.php"
+          class="round-border btn waves-effect waves-light yellow darken-2 black-text m2">Buy
+          Banners Here
+          <i class="material-icons black-text right">chevron_right</i>
+        </a>
+    </div>
+  </div>
+
+  <!-- CONTAINER -->
+  <div class="container">
+
+    <!-- MAIN -->
+    <main class="lf-main">
+
+      <!-- BREADCRUMBS -->
+      <nav class="yellow darken-2 m2a">
+        <div class="nav-wrapper ms2">
+          <div class="col s12">
+            <a href="/index.php" class="breadcrumb"><i class="material-icons">home</i></a>
+            <a href="/large_format.php" class="breadcrumb black-text">Large Format</a>
+          </div>
+        </div>
+      </nav>
+
+      <!-- PRODUCTS -->
+      <div id="category1" class="col s12">
+        <h4>Large Scale Marketing Made Easy.</h4>
+        <div class="row">
+          <div class="col s6">
+            <img src="img/vinyl_stickers.png" alt="printing vinyl stickers" class="responsive-img">
+          </div>
+          <div class="col s6">
+            <ul class="collection">
+              <a href="/large_format/banners.php" class="black-text waves-effect waves-light collection-item">
+                <li>Banners</li>
+              </a>
+              <a href="/large_format/magnets.php" class="collection-item black-text waves-effect waves-light">
+                <li>Magnets</li>
+              </a>
+              <a href="/large_format/decals.php" class="collection-item black-text waves-effect waves-light">
+                <li>Decals</li>
+              </a>
+              <a href="/large_format/vehicle_wraps.php" class="collection-item black-text waves-effect waves-light">
+                <li>Vehicle Wraps</li>
+              </a>
+              <a href="/large_format/window_perf.php" class="collection-item black-text waves-effect waves-light">
+                <li>Window Perf</li>
+              </a>
+              <a href="/large_format/displays.php" class="collection-item black-text waves-effect waves-light">
+                <li>Displays</li>
+              </a>
+              <a href="/large_format/signage.php" class="collection-item black-text waves-effect waves-light">
+                <li>Signage</li>
+              </a>
+              <a href="/large_format/posters.php" class="collection-item black-text waves-effect waves-light">
+                <li>Posters</li>
+              </a>
+            </ul>
+          </div>
+        </div>
+      </div> <!-- END PRODUCTS -->
+
+    </main>
+
+    <!-- INFO DROPDOWNS -->
+    <section class="info row">
+      <div class="col s12">
+        <h5>Some Frequently Asked Questions about Large Format Printing</h5>
+      </div>
+      <div class="col s12 m6">
+        <ul class="collapsible">
+          <li>
+            <div class="collapsible-header"><i class="material-icons teal-text">arrow_drop_down_circle</i>What is Large
+              Format Printing?</div>
+            <div class="collapsible-body"><span>Large format, or wide format, printing is handled on printers between
+                18" and 100" in width.
+                When getting larger than 100" it becomes referred to as super wide or grand format printing. It is
+                commonly done with solvent
+                inks, eco-solvent inks, or dye-sublimation on a large variety of substrates from banner material,
+                canvas, adhesive vinyls,
+                perforated window vision, wallpaper, vehicle wrap, and more. The Prints can be used indoors and outdoors
+                depending on the substrate used</span></div>
+          </li>
+          <li>
+            <div class="collapsible-header"><i class="material-icons teal-text">arrow_drop_down_circle</i>Is it
+              Eco-friendly?</div>
+            <div class="collapsible-body"><span>“Eco-solvent” inks are made with ether extracts from refined mineral
+                oils and have a
+                low VOC content. Eco-solvent inks allow for indoor printing without the need for ventilation systems
+                needed with traditional solvent inks.
+                Upon dying, the inks may be safely discarded.</span></div>
+          </li>
+          <li>
+            <div class="collapsible-header"><i class="material-icons teal-text">arrow_drop_down_circle</i>Can I use it
+              indoors?</div>
+            <div class="collapsible-body"><span>With the use of eco-solvent inks, you can safely use any of the products
+                indoors, and depending on the material used, it can often be safely used outdoors for years to
+                come.</span></div>
+          </li>
+        </ul>
+      </div>
+      <div class="col s12 m6">
+        <ul class="collapsible">
+          <li>
+            <div class="collapsible-header"><i class="material-icons teal-text">arrow_drop_down_circle</i>How large can
+              it go?</div>
+            <div class="collapsible-body"><span>We have a 5ft wide printer, meaning you can get up to 4'6" wide by the
+                length of the material (may media come in as long as 150" roles). If you need wider prints, you can
+                request a custom bid</span></div>
+          </li>
+          <li>
+            <div class="collapsible-header"><i class="material-icons teal-text">arrow_drop_down_circle</i>Can I get it
+              installed?</div>
+            <div class="collapsible-body"><span>If you are in the Northern Nevada area, we can do installations based on
+                location and job size. Request a custom bid for more details</span></div>
+          </li>
+          <li>
+            <div class="collapsible-header"><i class="material-icons teal-text">arrow_drop_down_circle</i>Can I get the
+              artwork created by you?</div>
+            <div class="collapsible-body"><span>If you do not have print ready files or a graphic designer, that is not
+                a problem. Let our designers handle your artwork needs.</span></div>
+          </li>
+        </ul>
+      </div>
+    </section>
+
+  </div> <!-- END CONTAINER -->
+
+
+
+  <?php include 'footer.php'; ?>
+
   <!--Import jQuery before materialize.js-->
   <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
   <script type="text/javascript" src="js/materialize.min.js"></script>
