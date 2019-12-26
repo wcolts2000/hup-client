0535da171a6b4b4e453302b35609a6ad3d8226d1
wcolts2000 <wcolts2000@gmail.com>
2019-12-25 19:21:08 -0800

    setting up baners layout

--- large_format/banners.php
+++ large_format/banners.php
@@ -14,6 +14,16 @@
 
 <body>
   <?php include '../header.php'; ?>
+  <!-- BREADCRUMBS -->
+  <nav class="yellow darken-2 m2a">
+    <div class="nav-wrapper ms2">
+      <div class="col s12">
+        <a href="/index.php" class="breadcrumb"><i class="material-icons">home</i></a>
+        <a href="/offset.php" class="breadcrumb black-text">Offset</a>
+      </div>
+    </div>
+  </nav>
+  <?php include '../footer.php'; ?>
   <!--Import jQuery before materialize.js-->
   <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
   <script type="text/javascript" src="../js/materialize.min.js"></script>
