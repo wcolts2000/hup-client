6f48e11dfc948aa35c37aefe2affa102cc654b07
wcolts2000 <wcolts2000@gmail.com>
2020-01-26 03:48:11 -0800

    calling shopping cart orders from db and displaying num in header and starting cart page

--- header.php
+++ header.php
@@ -1,198 +1,223 @@
 <nav class='yellow'>
   <!-- <div class='container'> -->
-    <div class='nav-wrapper ps2'>
-      
-      <!-- LOGO -->
-      <a href="/" class="brand-logo">
-        <img class="left" src="/img/hupLogoDrop.svg" width="30px" height="60px" alt="Hurry Up Print logo icon">
-      </a>
-      <ul class="right ps2">
-        <li id="cart-link" >
-          <a class='black-text' href='#!'>
-            <i class='material-icons'>shopping_cart</i>
-          </a>
-          <p id="cart">0</p>
-        </li>
-      </ul>
+  <div class='nav-wrapper ps2'>
 
-      <!-- HAMBURGER -->
-      <a class='button-collapse black-text' data-activates='mobile-nav' href='#'>
-        <i class='material-icons'>menu</i>
-      </a>
+    <!-- LOGO -->
+    <a href="/" class="brand-logo">
+      <img class="left" src="/img/hupLogoDrop.svg" width="30px" height="60px" alt="Hurry Up Print logo icon">
+    </a>
+    <ul class="right ps2">
+      <li id="cart-link">
+        <a class='black-text' href='/shopping_cart.php'>
+          <i class='material-icons'>shopping_cart</i>
+        </a>
+        <p id="cart">
+          <?php
+          if (isset($_SESSION['id'])) {
+            if (file_exists('includes/dbh.inc.php')) {
+              $filepath = 'includes/dbh.inc.php';
+            } else {
+              $filepath = '../includes/dbh.inc.php';
+            }
+            require_once $filepath;
+            $userId = $_SESSION['id'];
+            $sql = "SELECT * FROM shopping_cart WHERE userId=$userId;";
+            $stmt = mysqli_stmt_init($conn);
+            if (!mysqli_stmt_prepare($stmt, $sql)) {
+              header("Location: ../" . $_SERVER['REQUEST_URI'] . ".php?error=sqlerror");
+              exit();
+            } else {
+              mysqli_stmt_execute($stmt);
+              mysqli_stmt_store_result($stmt);
+              $itemCount = mysqli_stmt_num_rows($stmt);
+              echo $itemCount;
+            }
+          } else {
+            echo "0";
+          }
+          ?>
+        </p>
+      </li>
+    </ul>
 
-      <!-- RIGHT NAV DESKTOP -->
-      <ul class='right hide-on-med-and-down'>
-        <li>
-          <a data-activates='lf-dropdown' class='dropdown-button black-text' href='#!'>
-            Large Format
-            <i class='material-icons right'>arrow_drop_down</i>
-          </a>
-        </li>
-        <li>
-          <a data-activates='off-dropdown' class='dropdown-button black-text' href='#!'>
-            Offset
-            <i class='material-icons right'>arrow_drop_down</i>
-          </a>
-        </li>
-        <li>
-          <a data-activates='gar-dropdown' class='dropdown-button black-text' href='#!'>
-            Garments
-            <i class='material-icons right'>arrow_drop_down</i>
-          </a>
-        </li>
-        <li>
-          <a data-activates='web-dropdown' class='dropdown-button black-text' href='#!'>
-            Websites
-            <i class='material-icons right'>arrow_drop_down</i>
-          </a>
-        </li>
-        <li>
-          <a data-activates='acct-dropdown' class='dropdown-button black-text' href='#!'>
-            <i class='material-icons tiny left'>person</i>
-            My Account
-            <i class='material-icons right'>arrow_drop_down</i>
-          </a>
-        </li>
-      </ul>
-      
+    <!-- HAMBURGER -->
+    <a class='button-collapse black-text' data-activates='mobile-nav' href='#'>
+      <i class='material-icons'>menu</i>
+    </a>
+
+    <!-- RIGHT NAV DESKTOP -->
+    <ul class='right hide-on-med-and-down'>
+      <li>
+        <a data-activates='lf-dropdown' class='dropdown-button black-text' href='#!'>
+          Large Format
+          <i class='material-icons right'>arrow_drop_down</i>
+        </a>
+      </li>
+      <li>
+        <a data-activates='off-dropdown' class='dropdown-button black-text' href='#!'>
+          Offset
+          <i class='material-icons right'>arrow_drop_down</i>
+        </a>
+      </li>
+      <li>
+        <a data-activates='gar-dropdown' class='dropdown-button black-text' href='#!'>
+          Garments
+          <i class='material-icons right'>arrow_drop_down</i>
+        </a>
+      </li>
+      <li>
+        <a data-activates='web-dropdown' class='dropdown-button black-text' href='#!'>
+          Websites
+          <i class='material-icons right'>arrow_drop_down</i>
+        </a>
+      </li>
+      <li>
+        <a data-activates='acct-dropdown' class='dropdown-button black-text' href='#!'>
+          <i class='material-icons tiny left'>person</i>
+          My Account
+          <i class='material-icons right'>arrow_drop_down</i>
+        </a>
+      </li>
+    </ul>
 
 
 
-      <!-- MOBILE SIDE NAV -->
-      <ul class='side-nav' id='mobile-nav'>
 
-        <!-- MY ACCOUNT -->
-        <li>
-          <ul class='collapsible collapsible-accordion'>
-            <li>
-              <a class='black-text collapsible-header' href='#!'>
-                <i class='material-icons left'>person</i>
-                My Account
-                <!-- <i class='material-icons'>arrow_drop_down</i> -->
-              </a>
-              <div class='collapsible-body'>
-                <ul>
-                  <?php
-                  if (isset($_SESSION['email'])) {
-                    if (file_exists('includes/logout.inc.php')) {
-                        $filepath = 'includes/logout.inc.php';
-                      } else {
-                        $filepath = '../includes/logout.inc.php';
-                      }
-                    echo '<li>
-                      <form action="'.htmlspecialchars($filepath).'" method="post">
+    <!-- MOBILE SIDE NAV -->
+    <ul class='side-nav' id='mobile-nav'>
+
+      <!-- MY ACCOUNT -->
+      <li>
+        <ul class='collapsible collapsible-accordion'>
+          <li>
+            <a class='black-text collapsible-header' href='#!'>
+              <i class='material-icons left'>person</i>
+              My Account
+              <!-- <i class='material-icons'>arrow_drop_down</i> -->
+            </a>
+            <div class='collapsible-body'>
+              <ul>
+                <?php
+                if (isset($_SESSION['email'])) {
+                  if (file_exists('includes/logout.inc.php')) {
+                    $filepath = 'includes/logout.inc.php';
+                  } else {
+                    $filepath = '../includes/logout.inc.php';
+                  }
+                  echo '<li>
+                      <form action="' . htmlspecialchars($filepath) . '" method="post">
                         <button class="btn waves-effect waves-light black-text yellow ms2" name="logout-submit"
                           type="submit">Logout</button>
                       </form>
                     </li>';
-                    if (isset($_SESSION['admin'])) {
-                      echo '<li>
+                  if (isset($_SESSION['admin'])) {
+                    echo '<li>
                               <a href="/admin.php"><button class="black-text btn waves-effect waves-light yellow">Admin Panel</button></a>
-                            </li>';    
-                      }
-                  } else {
-                    echo "<li><a href='/login.php'>Login</a></li>                  
-                      <li><a href='/register.php'>Register</a></li>";
+                            </li>';
                   }
-                  ?>
-                </ul>
-              </div>
-            </li>
-          </ul>
-        </li>
+                } else {
+                  echo "<li><a href='/login.php'>Login</a></li>                  
+                      <li><a href='/register.php'>Register</a></li>";
+                }
+                ?>
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </li>
 
-        <!-- LARGE FORMAT -->
-        <li>
-          <ul class='collapsible collapsible-accordion'>
-            <li>
-              <a class='black-text collapsible-header' href='#!'>
-                <i class='material-icons left'>aspect_ratio</i>
-                Large Format
-              </a>
-              <div class='collapsible-body'>
-                <ul>
-                  <li class='yellow '><a href='/large_format.php'>Large Format</a></li>
-                  <li><a href='/large_format/banners.php'>Banners</a></li>
-                  <li><a href='/large_format/decals.php'>Decals</a></li>
-                  <li><a href='/large_format/displays.php'>Displays</a></li>
-                  <li><a href='/large_format/magnets.php'>Magnets</a></li>
-                  <li><a href='/large_format/posters.php'>Posters</a></li>
-                  <li><a href='/large_format/signage.php'>Signage</a></li>
-                  <li><a href='/large_format/vehicle_wraps.php'>Vehicle Wraps</a></li>
-                  <li><a href='/large_format/window_perf.php'>Window Perf</a></li>
-                </ul>
-              </div>
-            </li>
-          </ul>
-        </li>
+      <!-- LARGE FORMAT -->
+      <li>
+        <ul class='collapsible collapsible-accordion'>
+          <li>
+            <a class='black-text collapsible-header' href='#!'>
+              <i class='material-icons left'>aspect_ratio</i>
+              Large Format
+            </a>
+            <div class='collapsible-body'>
+              <ul>
+                <li class='yellow '><a href='/large_format.php'>Large Format</a></li>
+                <li><a href='/large_format/banners.php'>Banners</a></li>
+                <li><a href='/large_format/decals.php'>Decals</a></li>
+                <li><a href='/large_format/displays.php'>Displays</a></li>
+                <li><a href='/large_format/magnets.php'>Magnets</a></li>
+                <li><a href='/large_format/posters.php'>Posters</a></li>
+                <li><a href='/large_format/signage.php'>Signage</a></li>
+                <li><a href='/large_format/vehicle_wraps.php'>Vehicle Wraps</a></li>
+                <li><a href='/large_format/window_perf.php'>Window Perf</a></li>
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </li>
 
-        <!-- OFFSET -->
-        <li>
-          <ul class='collapsible collapsible-accordion'>
-            <li>
-              <a class='black-text collapsible-header' href='#!'>
-                <i class='material-icons left'>print</i>
-                Offset
-              </a>
-              <div class='collapsible-body'>
-                <ul>
-                  <li class='yellow '><a href='/offset.php'>Offset</a></li>
-                  <li><a href='/offset/business_cards.php'>Business Cards</a></li>
-                  <li><a href='/offset/flyers.php'>Flyers</a></li>
-                  <li><a href='/offset/invitations.php'>Invitations</a></li>
-                  <li><a href='/offset/menus.php'>Menus</a></li>
-                  <li><a href='/offset/postcards.php'>Postcards</a></li>
-                </ul>
-              </div>
-            </li>
-          </ul>
-        </li>
+      <!-- OFFSET -->
+      <li>
+        <ul class='collapsible collapsible-accordion'>
+          <li>
+            <a class='black-text collapsible-header' href='#!'>
+              <i class='material-icons left'>print</i>
+              Offset
+            </a>
+            <div class='collapsible-body'>
+              <ul>
+                <li class='yellow '><a href='/offset.php'>Offset</a></li>
+                <li><a href='/offset/business_cards.php'>Business Cards</a></li>
+                <li><a href='/offset/flyers.php'>Flyers</a></li>
+                <li><a href='/offset/invitations.php'>Invitations</a></li>
+                <li><a href='/offset/menus.php'>Menus</a></li>
+                <li><a href='/offset/postcards.php'>Postcards</a></li>
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </li>
 
-        <!-- GARMENTS -->
-        <li>
-          <ul class='collapsible collapsible-accordion'>
-            <li>
-              <a class='black-text collapsible-header' href='#!'>
-                <i class='material-icons left'>style</i>
-                Garments
-              </a>
-              <div class='collapsible-body'>
-                <ul>
-                  <li class='yellow '><a href='/garments.php'>Garments</a></li>
-                  <li><a href='/garments/embroidery.php'>Embroidery</a></li>
-                  <li><a href='/garments/silk_screen.php'>Silk Screen</a></li>
-                  <li><a href='/garments/vinyl.php'>Vinyl</a></li>
-                </ul>
-              </div>
-            </li>
-          </ul>
-        </li>
+      <!-- GARMENTS -->
+      <li>
+        <ul class='collapsible collapsible-accordion'>
+          <li>
+            <a class='black-text collapsible-header' href='#!'>
+              <i class='material-icons left'>style</i>
+              Garments
+            </a>
+            <div class='collapsible-body'>
+              <ul>
+                <li class='yellow '><a href='/garments.php'>Garments</a></li>
+                <li><a href='/garments/embroidery.php'>Embroidery</a></li>
+                <li><a href='/garments/silk_screen.php'>Silk Screen</a></li>
+                <li><a href='/garments/vinyl.php'>Vinyl</a></li>
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </li>
 
-        <!-- WEBSITES -->
-        <li>
-          <ul class='collapsible collapsible-accordion'>
-            <li>
-              <a class='black-text collapsible-header' href='#!'>
-                <i class='material-icons left'>important_devices</i>
-                Websites
-              </a>
-              <div class='collapsible-body'>
-                <ul>
-                  <li class='yellow '><a href='/websites.php'>Websites</a></li>
-                  <li><a href='/websites/applications.php'>Applications</a></li>
-                  <li><a href='/websites/blog_sites.php'>Blog Sites</a></li>
-                  <li><a href='/websites/dynamic_sites.php'>Dynamic Websites</a></li>
-                  <li><a href='/websites/e-commerce_sites.php'>E-commerce Sites</a></li>
-                  <li><a href='/websites/restaurant_sites.php'>Restaurant Sites</a></li>
-                  <li><a href='/websites/static_sites.php'>Static Websites</a></li>
-                </ul>
-              </div>
-            </li>
-          </ul>
-        </li>
-      </ul>
+      <!-- WEBSITES -->
+      <li>
+        <ul class='collapsible collapsible-accordion'>
+          <li>
+            <a class='black-text collapsible-header' href='#!'>
+              <i class='material-icons left'>important_devices</i>
+              Websites
+            </a>
+            <div class='collapsible-body'>
+              <ul>
+                <li class='yellow '><a href='/websites.php'>Websites</a></li>
+                <li><a href='/websites/applications.php'>Applications</a></li>
+                <li><a href='/websites/blog_sites.php'>Blog Sites</a></li>
+                <li><a href='/websites/dynamic_sites.php'>Dynamic Websites</a></li>
+                <li><a href='/websites/e-commerce_sites.php'>E-commerce Sites</a></li>
+                <li><a href='/websites/restaurant_sites.php'>Restaurant Sites</a></li>
+                <li><a href='/websites/static_sites.php'>Static Websites</a></li>
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </li>
+    </ul>
 
-    </div>
+  </div>
   <!-- </div> -->
 </nav>
 
@@ -239,15 +264,15 @@
     } else {
       $filepath = '../includes/logout.inc.php';
     }
-  echo '<li>
-          <form action="'.htmlspecialchars($filepath).'" method="post">
+    echo '<li>
+          <form action="' . htmlspecialchars($filepath) . '" method="post">
             <button class="btn waves-effect waves-light black-text yellow m1c" name="logout-submit" type="submit">Logout</button>
           </form>
         </li>';
-  if (isset($_SESSION['admin'])) {
-    echo '<li>
+    if (isset($_SESSION['admin'])) {
+      echo '<li>
     <a href="/admin.php"><button class="btn black-text waves-effect waves-light yellow">Admin</button></a>
-  </li>';    
+  </li>';
     }
   } else {
     echo "<li><a href='/login.php'>Login</a></li>  
---
+++ shopping_cart.php
@@ -0,0 +1,39 @@
+<?php $title = "Shopping Cart" ?>
+<?php $description = "Hurry Up Print customer shopping cart" ?>
+<?php require('head.php') ?>
+<?php include 'header.php'; ?>
+
+<!-- CONTAINER -->
+<div class="container">
+
+  <!-- MAIN -->
+  <main class="lf-main">
+
+    <!-- PRODUCTS -->
+    <div class="col s12">
+      <table>
+        <thead>
+          <tr>
+            <th>Product</th>
+            <th>options</th>
+            <th>Price</th>
+            <th>Quantity</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>banners</td>
+            <td>options will go here somehow</td>
+            <td>$0.87</td>
+            <td>2</td>
+          </tr>
+        </tbody>
+      </table>
+    </div> <!-- END PRODUCTS -->
+
+  </main>
+
+</div> <!-- END CONTAINER -->
+
+<?php include 'footer.php' ?>
