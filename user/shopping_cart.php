1785c5a6e6fce0af4396b5515921bf1ff195884a
wcolts2000 <wcolts2000@gmail.com>
2020-02-01 09:56:48 -0800

    stytlng shopping cart layout

--- user/shopping_cart.php
+++ user/shopping_cart.php
@@ -5,33 +5,48 @@
 
 <!-- CONTAINER -->
 <div class="container">
+  <div class="cart-header">
+    <h1 class="cart-heading">Shopping Cart</h1>
+    <p class="cart-price red-text">Price</p>
+  </div>
+  <div class="divider"></div>
 
   <!-- MAIN -->
-  <main class="lf-main">
+  <main class="row">
+    <div class="col s12 m12 l8 cart-left mb1">
+      <!-- PRODUCTS -->
+      <div class="col s12">
+        <table>
+          <thead>
+            <tr>
+              <th>Product</th>
+              <th>options</th>
+              <th>Price</th>
+              <th>Quantity</th>
+              <th class="red-text">Remove</th>
+            </tr>
+          </thead>
 
-    <!-- PRODUCTS -->
-    <div class="col s12">
-      <table>
-        <thead>
-          <tr>
-            <th>Product</th>
-            <th>options</th>
-            <th>Price</th>
-            <th>Quantity</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          <tr>
-            <td>banners</td>
-            <td>options will go here somehow</td>
-            <td>$0.87</td>
-            <td>2</td>
-          </tr>
-        </tbody>
-      </table>
-    </div> <!-- END PRODUCTS -->
-
+          <tbody>
+            <tr>
+              <td>banners</td>
+              <td>options will go here somehow</td>
+              <td>$0.87</td>
+              <td>2</td>
+              <td class="red-text"><i class="material-icons">delete_forever</i></td>
+            </tr>
+          </tbody>
+        </table>
+      </div> <!-- END PRODUCTS -->
+    </div>
+    <div class="col s12 m12 l4 cart-right mb1">
+      <p>Total: 10.00</p>
+    </div>
+    <div class="cart-footer">
+      <div class="divider"></div>
+      <p class="cart-subtotal">subtotal:</p>
+      <p class="cart-total red-text">Price</p>
+    </div>
   </main>
 
 </div> <!-- END CONTAINER -->
